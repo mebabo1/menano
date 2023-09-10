2d39ab01cae104db617bc98ed0e7d53c17689bc6
mebabo1 <mebsbo1122@gmail.com>
2023-09-10 21:25:39 +0900

    Update install04.sh

--- install04.sh
+++ install04.sh
@@ -24,7 +24,16 @@ deb-src http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted univ
 " > /etc/apt/sources.list
 
 dpkg --add-architecture armhf
-apt update
+echo " system update"
+apt update &>/dev/null
+echo " pkg install arm64"
+sudo apt install -y libxrender1 libxfixes3 libxrandr2 libxcomposite1 libxi6 libxcursor1
+sudo apt install -y libgnutls30 libxext-dev libasound2 libvulkan1 libfontconfig-dev libfreetype6-dev libpulse0 libasound2-plugins
+echo " pkg install armhf"
+sudo apt install -y libxrender1:armhf libxfixes3:armhf libxrandr2:armhf libxcomposite1:armhf libxi6:armhf libxcursor1:armhf
+sudo apt install -y libgnutls30:armhf libxext-dev:armhf libasound2:armhf libvulkan1:armhf libfontconfig-dev:armhf libfreetype6-dev:armhf libpulse0:armhf libasound2-plugins:armhf
+
+
 
 wget https://github.com/mebabo1/menano/releases/download/bininstall/usr.tar.gz
 tar xf usr.tar.gz
