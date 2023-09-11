4d8b6f2bdbd6cd7519db1a0895b83f98bbbaaae2
mebabo1 <mebsbo1122@gmail.com>
2023-09-11 10:53:14 +0900

    Update install04.sh

--- install04.sh
+++ install04.sh
@@ -2,11 +2,11 @@
 
 # Preparing
 
-echo " system update"
+echo " system update 10"
 apt update &>/dev/null
-echo " pkg install sudo nano wget"
+echo " pkg install sudo nano wget 09"
 apt install sudo nano wget -y &>/dev/null
-echo " pkg install language"
+echo " pkg install language 08"
 apt install language-pack-ko language-pack-ja language-pack-en fonts-nanum fonts-nanum-coding fonts-nanum-extra -y &>/dev/null
 
 echo "
@@ -24,28 +24,28 @@ deb-src http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted univ
 " > /etc/apt/sources.list
 
 dpkg --add-architecture armhf
-echo " system update"
+echo " system update 07"
 apt update &>/dev/null
-echo " pkg install arm64 01"
+echo " pkg install arm64 06"
 sudo apt install -y libxrender1 libxfixes3 libxrandr2 libxcomposite1 libxi6 libxcursor1 &>/dev/null
-echo " pkg install arm64 02"
+echo " pkg install arm64 05"
 sudo apt install -y libgnutls30 libxext-dev libasound2 libvulkan1 libfontconfig-dev libfreetype6-dev libpulse0 libasound2-plugins &>/dev/null
-echo " pkg install armhf 01"
+echo " pkg install armhf 04"
 sudo apt install -y libxrender1:armhf libxfixes3:armhf libxrandr2:armhf libxcomposite1:armhf libxi6:armhf libxcursor1:armhf &>/dev/null
-echo " pkg install armhf 02"
+echo " pkg install armhf 03"
 sudo apt install -y libgnutls30:armhf libxext-dev:armhf libasound2:armhf libvulkan1:armhf libfontconfig-dev:armhf libfreetype6-dev:armhf libpulse0:armhf libasound2-plugins:armhf &>/dev/null
-echo " install optFile"
+echo " install optFile 02"
 wget https://github.com/mebabo1/menano/releases/download/bininstall/opt.tar.gz &>/dev/null
 rm -rf .bashrc
 tar xf opt.tar.gz
 cp -r opt /
 rm -rf opt.tar.gz opt
-echo " install amd64File"
+echo " install amd64File 01"
 wget https://github.com/mebabo1/menano/releases/download/bininstall/usr.tar.gz &>/dev/null
 tar xf usr.tar.gz
 cp -r usr /
 rm -rf usr.tar.gz usr
-echo " install Box64Droid"
+echo " install Box64Droid 00"
 cd /sdcard/
 wget https://github.com/mebabo1/menano/releases/download/Box/Box64Droid.tar.gz &>/dev/null
 tar xf Box64Droid.tar.gz
