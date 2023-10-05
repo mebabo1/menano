1188af6c729c43c4d6b6b7ad0a4d26bf5957df81
mebabo1 <mebsbo1122@gmail.com>
2023-10-06 08:00:05 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Set the countdown timer (e.g., 60 seconds)
+countdown=60
+
+# Function to display countdown in hh:mm:ss format
+function display_countdown() {
+    local hours=$((countdown / 3600))
+    local minutes=$(( (countdown % 3600) / 60 ))
+    local seconds=$((countdown % 60))
+    echo -ne "Time remaining: $hours:$minutes:$seconds\r"
+}
+
+# Start time
+start_time=$(date +%s)
+
+# Display a waiting message
+echo "Executing... Please wait."
+
+# Execute the commands
+pkg update -y
+clear
+# Display a waiting message
+echo "Executing... Please wait."
+
+pkg install x11-repo -y > /dev/null 2>&1
+pkg install proot-distro proot termux-x11-nightly wget git pulseaudio -y > /dev/null 2>&1
+termux-setup-storage > /dev/null 2>&1
+
+chmod +x start-droidbox && chmod +x droidbox > /dev/null 2>&1
+cp -r droidbox start-droidbox /data/data/com.termux/files/usr/bin > /dev/null 2>&1
+rm -rf droidbox start-droidbox > /dev/null 2>&1
+
+# Countdown timer synchronized with execution
+while [ $countdown -gt 0 ]; do
+    display_countdown
+    sleep 1
+    ((countdown--))
+done
+
+# Clear the countdown message
+echo -ne "\r\033[K"
+
+# Completion message
+echo "Execution completed successfully!"
