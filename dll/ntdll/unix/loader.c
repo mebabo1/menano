d4ad54437df696c87dc15697b273049b6a2b2308
mebabo1 <mebsbo1122@gmail.com>
2026-03-27 15:47:52 +0900

    Update loader.c

--- dll/ntdll/unix/loader.c
+++ dll/ntdll/unix/loader.c
@@ -366,7 +366,6 @@ static const char *get_pe_dir( WORD machine )
     case IMAGE_FILE_MACHINE_AMD64: return "/aarch64-windows";
     case IMAGE_FILE_MACHINE_AMD64: return "/x86_64-windows";
     case IMAGE_FILE_MACHINE_ARMNT: return "/arm-windows";
-    case IMAGE_FILE_MACHINE_ARM64: return "/aarch64-windows";
     default: return "";
     }
 }
