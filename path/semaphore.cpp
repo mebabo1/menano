5bceb76080108208f6f79ff457b4088b29ce5d01
mebabo1 <mebsbo1122@gmail.com>
2026-05-11 22:07:11 +0900

    Update semaphore.cpp

--- path/semaphore.cpp
+++ path/semaphore.cpp
@@ -9,6 +9,7 @@
 using namespace Mini;
 
 Semaphore::Semaphore(VkDevice device) {
+    // create semaphore
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
     };
@@ -25,6 +26,7 @@ Semaphore::Semaphore(VkDevice device) {
     if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
         throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
+    // store semaphore in shared ptr
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
         [dev = device](VkSemaphore* semaphoreHandle) {
@@ -34,18 +36,11 @@ Semaphore::Semaphore(VkDevice device) {
 }
 
 Semaphore::Semaphore(VkDevice device, int* fd) {
-
-    // Linux/Turnip friendly semaphore export
-    const VkExportSemaphoreCreateInfo exportInfo{
-        .sType = VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
-
-        // CHANGE THIS
-        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
-    };
+    // Termux glibc / Turnip fallback:
+    // disable external semaphore fd export completely
 
     const VkSemaphoreCreateInfo desc{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = &exportInfo
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
     };
 
     VkSemaphore semaphoreHandle{};
@@ -60,19 +55,11 @@ Semaphore::Semaphore(VkDevice device, int* fd) {
     if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
         throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
-    const VkSemaphoreGetFdInfoKHR fdInfo{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
-        .semaphore = semaphoreHandle,
+    // no external fd export
+    if (fd)
+        *fd = -1;
 
-        // CHANGE THIS TOO
-        .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
-    };
-
-    res = Layer::ovkGetSemaphoreFdKHR(device, &fdInfo, fd);
-
-    if (res != VK_SUCCESS || *fd < 0)
-        throw LSFG::vulkan_error(res, "Unable to export semaphore to fd");
-
+    // store semaphore in shared ptr
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
         [dev = device](VkSemaphore* semaphoreHandle) {
