8aa5827f4b5c0eaa9951e2a7ff1e4cd9f3ec7b0b
mebabo1 <mebsbo1122@gmail.com>
2026-05-11 21:58:51 +0900

    Update semaphore.cpp

--- path/semaphore.cpp
+++ path/semaphore.cpp
@@ -3,57 +3,80 @@
 #include "layer.hpp"
 
 #include <vulkan/vulkan_core.h>
+
 #include <memory>
 
 using namespace Mini;
 
-// 기본 생성자
 Semaphore::Semaphore(VkDevice device) {
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
     };
+
     VkSemaphore semaphoreHandle{};
-    auto res = Layer::ovkCreateSemaphore(device, &desc, nullptr, &semaphoreHandle);
-    
-    if (res != VK_SUCCESS)
-        throw LSFG::vulkan_error(res, "Failed to create basic semaphore");
+
+    auto res = Layer::ovkCreateSemaphore(
+        device,
+        &desc,
+        nullptr,
+        &semaphoreHandle
+    );
+
+    if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
+        throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
-        [dev = device](VkSemaphore* h) {
-            if (h && *h != VK_NULL_HANDLE) {
-                Layer::ovkDestroySemaphore(dev, *h, nullptr);
-            }
-            delete h;
+        [dev = device](VkSemaphore* semaphoreHandle) {
+            Layer::ovkDestroySemaphore(dev, *semaphoreHandle, nullptr);
         }
     );
 }
 
-// FD를 인자로 받는 생성자 (glibc 우회 버전)
 Semaphore::Semaphore(VkDevice device, int* fd) {
-    // 안드로이드 전용 SYNC_FD 확장을 완전히 제거합니다.
+
+    // Linux/Turnip friendly semaphore export
+    const VkExportSemaphoreCreateInfo exportInfo{
+        .sType = VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
+
+        // CHANGE THIS
+        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
+    };
+
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = nullptr
+        .pNext = &exportInfo
     };
-    
-    VkSemaphore semaphoreHandle{};
-    auto res = Layer::ovkCreateSemaphore(device, &desc, nullptr, &semaphoreHandle);
-    
-    if (res != VK_SUCCESS)
-        throw LSFG::vulkan_error(res, "Failed to create semaphore");
 
-    // glibc 환경에서는 FD 추출이 불가능하므로 항상 -1로 설정합니다.
-    // 대신 프레임 생성기는 이 세마포어 핸들 자체를 Vulkan API 내에서 사용하게 됩니다.
-    if (fd) *fd = -1;
+    VkSemaphore semaphoreHandle{};
+
+    auto res = Layer::ovkCreateSemaphore(
+        device,
+        &desc,
+        nullptr,
+        &semaphoreHandle
+    );
+
+    if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
+        throw LSFG::vulkan_error(res, "Unable to create semaphore");
+
+    const VkSemaphoreGetFdInfoKHR fdInfo{
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
+        .semaphore = semaphoreHandle,
+
+        // CHANGE THIS TOO
+        .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
+    };
+
+    res = Layer::ovkGetSemaphoreFdKHR(device, &fdInfo, fd);
+
+    if (res != VK_SUCCESS || *fd < 0)
+        throw LSFG::vulkan_error(res, "Unable to export semaphore to fd");
 
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
-        [dev = device](VkSemaphore* h) {
-            if (h && *h != VK_NULL_HANDLE) {
-                Layer::ovkDestroySemaphore(dev, *h, nullptr);
-            }
-            delete h;
+        [dev = device](VkSemaphore* semaphoreHandle) {
+            Layer::ovkDestroySemaphore(dev, *semaphoreHandle, nullptr);
         }
     );
 }
