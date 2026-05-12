0a6e4f96879eff3825bdeab0fb0541a64a3752f2
mebabo1 <mebsbo1122@gmail.com>
2026-05-12 16:29:48 +0900

    Update semaphore.cpp

--- path/semaphore.cpp
+++ path/semaphore.cpp
@@ -11,140 +11,166 @@ using namespace Mini;
 
 Semaphore::Semaphore(VkDevice device) {
 
+    /*
+     * Normal Vulkan semaphore
+     */
+
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
     };
 
     VkSemaphore semaphoreHandle{};
 
-    auto res = Layer::ovkCreateSemaphore(
-        device,
-        &desc,
-        nullptr,
-        &semaphoreHandle
-    );
-
-    if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE) {
-        throw LSFG::vulkan_error(res, "Unable to create semaphore");
-    }
-
-    this->semaphore =
-        std::shared_ptr<VkSemaphore>(
-            new VkSemaphore(semaphoreHandle),
-            [dev = device](VkSemaphore* semaphoreHandle) {
-                Layer::ovkDestroySemaphore(
-                    dev,
-                    *semaphoreHandle,
-                    nullptr
-                );
-            }
-        );
-}
-
-Semaphore::Semaphore(VkDevice device, int* fd) {
-
-    bool externalSupported = true;
-
-    const VkExportSemaphoreCreateInfo exportInfo{
-        .sType = VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
-        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
-    };
-
-    const VkSemaphoreCreateInfo desc{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = &exportInfo
-    };
-
-    VkSemaphore semaphoreHandle{};
-
-    auto res = Layer::ovkCreateSemaphore(
-        device,
-        &desc,
-        nullptr,
-        &semaphoreHandle
-    );
-
-    /*
-     * =========================================================
-     * CASE 1: External semaphore supported (ideal path)
-     * =========================================================
-     */
-    if (res == VK_SUCCESS && semaphoreHandle != VK_NULL_HANDLE) {
-
-        const VkSemaphoreGetFdInfoKHR fdInfo{
-            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
-            .semaphore = semaphoreHandle,
-            .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
-        };
-
-        res = Layer::ovkGetSemaphoreFdKHR(device, &fdInfo, fd);
-
-        if (res != VK_SUCCESS || !fd || *fd < 0) {
-
-            std::cerr
-                << "lsfg-vk: external fd export failed -> switching to internal LSFG sync path"
-                << std::endl;
-
-            externalSupported = false;
-
-            if (fd)
-                *fd = -1;
-        }
-    }
-    else {
-        externalSupported = false;
-    }
-
-    /*
-     * =========================================================
-     * CASE 2: Fallback BUT NOT DISABLE LSFG
-     * =========================================================
-     * Instead of disabling LSFG:
-     * -> mark fd as invalid
-     * -> LSFG will use internal sync mode in context.cpp
-     */
-    if (!externalSupported) {
-
-        std::cerr
-            << "lsfg-vk: semaphore fallback -> INTERNAL LSFG MODE ACTIVE"
-            << std::endl;
-
-        const VkSemaphoreCreateInfo fallbackDesc{
-            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
-        };
-
-        res = Layer::ovkCreateSemaphore(
+    auto res =
+        Layer::ovkCreateSemaphore(
             device,
-            &fallbackDesc,
+            &desc,
             nullptr,
-            &semaphoreHandle
-        );
+            &semaphoreHandle);
 
-        if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE) {
-            throw LSFG::vulkan_error(res, "Unable to create fallback semaphore");
-        }
+    if (res != VK_SUCCESS ||
+        semaphoreHandle == VK_NULL_HANDLE) {
 
-        /*
-         * IMPORTANT:
-         * fd = -1 does NOT mean LSFG off anymore
-         * it means "use internal sync path"
-         */
-        if (fd)
-            *fd = -1;
+        throw LSFG::vulkan_error(
+            res,
+            "Unable to create semaphore");
     }
 
     /*
      * Store semaphore
      */
+
     this->semaphore =
         std::shared_ptr<VkSemaphore>(
             new VkSemaphore(semaphoreHandle),
-            [dev = device](VkSemaphore* semaphoreHandle) {
+            [dev = device](
+                VkSemaphore* semaphoreHandle) {
+
                 Layer::ovkDestroySemaphore(
                     dev,
                     *semaphoreHandle,
-                    nullptr
-                );
-            }
-        );
+                    nullptr);
+            });
+}
+
+Semaphore::Semaphore(VkDevice device, int* fd) {
+
+    /*
+     * Try exportable semaphore first.
+     *
+     * Turnip/Termux may support this partially.
+     */
+
+    const VkExportSemaphoreCreateInfo exportInfo{
+        .sType =
+            VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
+        .handleTypes =
+            VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
+    };
+
+    const VkSemaphoreCreateInfo desc{
+        .sType =
+            VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+        .pNext = &exportInfo
+    };
+
+    VkSemaphore semaphoreHandle{};
+
+    auto res =
+        Layer::ovkCreateSemaphore(
+            device,
+            &desc,
+            nullptr,
+            &semaphoreHandle);
+
+    /*
+     * Fallback:
+     * create normal semaphore if exportable
+     * semaphore creation fails.
+     */
+
+    if (res != VK_SUCCESS ||
+        semaphoreHandle == VK_NULL_HANDLE) {
+
+        std::cerr
+            << "lsfg-vk: external semaphore unsupported, using fallback"
+            << std::endl;
+
+        const VkSemaphoreCreateInfo fallbackDesc{
+            .sType =
+                VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
+        };
+
+        res =
+            Layer::ovkCreateSemaphore(
+                device,
+                &fallbackDesc,
+                nullptr,
+                &semaphoreHandle);
+
+        if (res != VK_SUCCESS ||
+            semaphoreHandle == VK_NULL_HANDLE) {
+
+            throw LSFG::vulkan_error(
+                res,
+                "Unable to create semaphore");
+        }
+
+        if (fd)
+            *fd = -1;
+    }
+    else {
+
+        /*
+         * Export semaphore FD
+         */
+
+        const VkSemaphoreGetFdInfoKHR fdInfo{
+            .sType =
+                VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
+            .semaphore = semaphoreHandle,
+            .handleType =
+                VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
+        };
+
+        res =
+            Layer::ovkGetSemaphoreFdKHR(
+                device,
+                &fdInfo,
+                fd);
+
+        /*
+         * Export failed.
+         *
+         * Keep semaphore alive anyway.
+         */
+
+        if (res != VK_SUCCESS ||
+            !fd ||
+            *fd < 0) {
+
+            std::cerr
+                << "lsfg-vk: semaphore fd export failed, fallback mode"
+                << std::endl;
+
+            if (fd)
+                *fd = -1;
+        }
+    }
+
+    /*
+     * Store semaphore
+     */
+
+    this->semaphore =
+        std::shared_ptr<VkSemaphore>(
+            new VkSemaphore(semaphoreHandle),
+            [dev = device](
+                VkSemaphore* semaphoreHandle) {
+
+                Layer::ovkDestroySemaphore(
+                    dev,
+                    *semaphoreHandle,
+                    nullptr);
+            });
 }
