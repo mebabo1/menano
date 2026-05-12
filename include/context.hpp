b52ca30642419209848e9d03e6d8ae82f33f1697
mebabo1 <mebsbo1122@gmail.com>
2026-05-12 22:27:13 +0900

    Update context.hpp

--- include/context.hpp
+++ include/context.hpp
@@ -13,23 +13,9 @@
 #include <memory>
 #include <vector>
 
-///
-/// This class is the frame generation context.
-/// There should be one instance per swapchain.
-///
 class LsContext {
 public:
 
-    ///
-    /// Create the swapchain context.
-    ///
-    /// @param info The device information to use.
-    /// @param swapchain The Vulkan swapchain to use.
-    /// @param extent The extent of the swapchain images.
-    /// @param swapchainImages The swapchain images to use.
-    ///
-    /// @throws LSFG::vulkan_error if any Vulkan call fails.
-    ///
     LsContext(
         const Hooks::DeviceInfo& info,
         VkSwapchainKHR swapchain,
@@ -37,19 +23,6 @@ public:
         const std::vector<VkImage>& swapchainImages
     );
 
-    ///
-    /// Custom present logic.
-    ///
-    /// @param info The device information to use.
-    /// @param pNext Unknown pointer set in the present info structure.
-    /// @param queue The Vulkan queue to present the frame on.
-    /// @param gameRenderSemaphores The semaphores to wait on before presenting.
-    /// @param presentIdx The index of the swapchain image to present.
-    ///
-    /// @return The result of the Vulkan present operation.
-    ///
-    /// @throws LSFG::vulkan_error if any Vulkan call fails.
-    ///
     VkResult present(
         const Hooks::DeviceInfo& info,
         const void* pNext,
@@ -58,11 +31,9 @@ public:
         uint32_t presentIdx
     );
 
-    // Non-copyable
     LsContext(const LsContext&) = delete;
     LsContext& operator=(const LsContext&) = delete;
 
-    // Moveable
     LsContext(LsContext&&) = default;
     LsContext& operator=(LsContext&&) = default;
 
@@ -76,10 +47,6 @@ private:
         size_t size{0};
     };
 
-    /*
-     * helper:
-     * upload shared memory to gpu image
-     */
     void uploadShmToGPU(
         void* src,
         Mini::Image& dst
@@ -87,6 +54,9 @@ private:
 
 private:
 
+    /*
+     * persistent device info
+     */
     Hooks::DeviceInfo info;
 
     VkSwapchainKHR swapchain;
@@ -95,8 +65,15 @@ private:
 
     VkExtent2D extent;
 
+    /*
+     * fallback mode:
+     * use shm upload when external fd fails
+     */
     bool useShmFallback{false};
 
+    /*
+     * shared memory buffers
+     */
     std::array<ShmBuffer, 2> shm;
 
     /*
@@ -105,13 +82,13 @@ private:
     std::shared_ptr<int32_t> lsfgCtxId;
 
     /*
-     * frames shared with lsfg
+     * input frames
      */
     Mini::Image frame_0;
     Mini::Image frame_1;
 
     /*
-     * output images shared with lsfg
+     * generated output frames
      */
     std::vector<Mini::Image> out_n;
 
@@ -121,51 +98,27 @@ private:
 
     struct RenderPassInfo {
 
-        /*
-         * copy swapchain -> frame_0/frame_1
-         */
         Mini::CommandBuffer preCopyBuf;
 
-        /*
-         * signal pre-copy done
-         */
         std::array<Mini::Semaphore, 2>
             preCopySemaphores;
 
-        /*
-         * signal lsfg done
-         */
         std::vector<Mini::Semaphore>
             renderSemaphores;
 
-        /*
-         * acquire semaphores
-         */
         std::vector<Mini::Semaphore>
             acquireSemaphores;
 
-        /*
-         * copy generated frame -> swapchain
-         */
         std::vector<Mini::CommandBuffer>
             postCopyBufs;
 
-        /*
-         * post-copy completion
-         */
         std::vector<Mini::Semaphore>
             postCopySemaphores;
 
-        /*
-         * ordering between presents
-         */
         std::vector<Mini::Semaphore>
             prevPostCopySemaphores;
     };
 
-    /*
-     * allocate 8 frames-in-flight
-     */
     std::array<RenderPassInfo, 8>
         passInfos;
 };
