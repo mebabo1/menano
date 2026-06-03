b710549d00abddc82f85a4c0d5e82dc239dbaf19
mebabo1 <mebsbo1122@gmail.com>
2026-06-03 12:46:28 +0900

    Update env.c

--- dlls/ntdll/unix/env.c
+++ dlls/ntdll/unix/env.c
@@ -729,23 +729,32 @@ static void init_locale(void)
     const NLS_LOCALE_HEADER *locale_table;
     const NLS_LOCALE_DATA *locale;
     char *p;
-
 #ifdef __ANDROID__
-    all = getenv( "LC_ALL" );
-    if (!all)
-        all = "C.UTF-8";
-
-    if (!unix_to_win_locale( all, system_locale )) system_locale[0] = 0;
-    TRACE_(nls)( "Unix LC_ALL is %s, setting system locale to %s\n", debugstr_a(all), debugstr_a(system_locale) );
-
-    if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
     {
-        FIXME( "HACK setting EN locale.\n" );
-        all = "en_US.UTF-8";
-    }
+        const char *all; // 👈 'all' 변수가 없다고 하니 내부 블록에서 직접 선언합니다.
+
+        all = getenv( "LC_ALL" );
+        if (!all)
+            all = "C.UTF-8";
+
+        if (!unix_to_win_locale( all, system_locale )) system_locale[0] = 0;
         
-    if (!unix_to_win_locale( all, user_locale )) user_locale[0] = 0;
-    TRACE_(nls)( "Unix LC_ALL is %s, user system locale to %s\n", debugstr_a(all), debugstr_a(user_locale) );
+        // 👈 컴파일 에러를 뿜는 TRACE_(nls) 대신 일반 표준 출력(fprintf)으로 대체합니다.
+        // TRACE_(nls)( "Unix LC_ALL is %s...", ... ); 
+        fprintf(stderr, "wine: Unix LC_ALL is %s, setting system locale to %s\n", all, system_locale);
+
+        if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
+        {
+            FIXME( "HACK setting EN locale.\n" );
+            all = "en_US.UTF-8";
+        }
+            
+        if (!unix_to_win_locale( all, user_locale )) user_locale[0] = 0;
+        
+        // 👈 마찬가지로 컴파일 에러 유발 구문을 안전하게 덮어씁니다.
+        // TRACE_(nls)( "Unix LC_ALL is %s...", ... );
+        fprintf(stderr, "wine: Unix LC_ALL is %s, user system locale to %s\n", all, user_locale);
+    }
 #else
     if (!unix_to_win_locale ( all, user_locale )) user_locale[0] = 0;     
     
@@ -768,7 +777,6 @@ static void init_locale(void)
     if (!unix_to_win_locale( messages, user_locale )) user_locale[0] = 0;
     TRACE_(nls)( "Unix LC_MESSAGES is %s, user system locale to %s\n", debugstr_a(messages), debugstr_a(user_locale) );
 #endif
-
 #ifdef __APPLE__
     if (!system_locale[0])
     {
