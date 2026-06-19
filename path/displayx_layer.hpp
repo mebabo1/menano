13692eb468446cf25d051c38d5a5169061569d5a
mebabo1 <mebsbo1122@gmail.com>
2026-06-20 08:37:07 +0900

    Update displayx_layer.hpp

--- path/displayx_layer.hpp
+++ path/displayx_layer.hpp
@@ -1,29 +1,28 @@
 #pragma once
 
-#include "vulkan/vk_layer.h"
-#include "logger.hpp"
-#include "id.hpp"
+// 1. 시스템 헤더 및 X11 관련 헤더를 반드시 Vulkan보다 먼저 include
+#include <xcb/xcb.h>
+#include <X11/Xlib.h>
+#include <X11/Xlib-xcb.h>
 
+// 2. Vulkan 관련 헤더 (순서 및 중복 정리)
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_layer.h>
 #include <vulkan/vulkan_xcb.h>
 #include <vulkan/vulkan_xlib.h>
-#include <vulkan/vulkan.h>
+
+// 3. 표준 라이브러리 및 프로젝트 의존성
 #include <unistd.h>
 #include <unordered_map>
 #include <android/hardware_buffer.h>
 #include <mutex>
-#include <X11/Xlib-xcb.h>
-#include <X11/Xlib.h>
-#include <xcb/xcb.h>
-#include <libsync.h>
-#include <vulkan/vulkan_xcb.h>
-#include <vulkan/vulkan_xlib.h>
 #include <vector>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <memory>
 #include <cstring>
+#include "logger.hpp"
+#include "id.hpp"
 
 // [수정] 링키지 오류 방지 및 심볼 가시성 확보
 #undef VK_LAYER_EXPORT
@@ -45,7 +44,7 @@ void* GetKey(T item) {
 #define VK_UNWRAP_NON_DISPATCHABLE_HANDLE(handle, type, variable) \
 	type *variable = (type *)(uintptr_t)handle;
 
-// 전역 상태 관리
+// 전역 상태 관리 (extern 선언)
 extern std::unordered_map<void *, VkLayerInstanceDispatchTable> instanceDispatch;
 extern std::unordered_map<void *, VkInstance> instanceMap;
 extern std::unordered_map<void *, std::shared_ptr<struct device>> deviceDispatch;                             
@@ -59,6 +58,7 @@ typedef std::lock_guard<std::mutex> scoped_lock;
 if (!strcmp(pName, "vk" #func)) \
     return (PFN_vkVoidFunction)&DisplayX_##func;
 
+// 구조체 정의
 struct fence {
 	VkFence handle;
 	int sync_fd;
@@ -80,7 +80,6 @@ struct device {
 struct fake_surface {
     uintptr_t loader_magic;
     VkObjectType obj_type;
-
     VkInstance instance;
     int native_renderer_fd;
     xcb_connection_t *conn;
@@ -98,11 +97,9 @@ struct fake_swapchain_image {
 struct fake_swapchain {
     uintptr_t loader_magic;
     VkObjectType obj_type;
-
     VkDevice wsi_device;
     bool wait_for_present;
     bool use_prime_blit;
-
     struct fake_surface *surface;
     std::shared_ptr<struct device> device;
     uint32_t imageCount;
@@ -113,11 +110,10 @@ struct fake_swapchain {
     std::vector<std::shared_ptr<struct fake_swapchain_image>> images;
     uint32_t currentImage;
     uint8_t id;
-    
-    // [추가] 동시 렌더링 상황에서 소켓 패킷 꼬임 방지용 뮤텍스
     std::mutex socket_mutex; 
 };
 
+// API 함수 선언 (매크로 사용)
 VK_LAYER_EXPORT VkResult VKAPI_CALL DisplayX_CreateXcbSurfaceKHR(VkInstance, const VkXcbSurfaceCreateInfoKHR*, const VkAllocationCallbacks*, VkSurfaceKHR*);
 VK_LAYER_EXPORT VkResult VKAPI_CALL DisplayX_CreateXlibSurfaceKHR(VkInstance, const VkXlibSurfaceCreateInfoKHR*, const VkAllocationCallbacks*, VkSurfaceKHR*);
 VK_LAYER_EXPORT void VKAPI_CALL DisplayX_DestroySurfaceKHR(VkInstance, VkSurfaceKHR, const VkAllocationCallbacks*);
