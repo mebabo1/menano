a5a3864633412f74d9a5825bc2ed1456f336be0b
mebabo1 <mebsbo1122@gmail.com>
2026-07-11 14:23:47 +0900

    Update 0env.c

--- dlls/ntdll/unix/0env.c
+++ dlls/ntdll/unix/0env.c
@@ -805,10 +805,47 @@ static void init_locale(void)
     const NLS_LOCALE_DATA *locale;
     char *p;
 
-    setlocale( LC_ALL, "" );
-    if (!unix_to_win_locale( setlocale( LC_CTYPE, NULL ), system_locale )) system_locale[0] = 0;
-    if (!unix_to_win_locale( setlocale( LC_MESSAGES, NULL ), user_locale )) user_locale[0] = 0;
+#ifdef __ANDROID__
+    {
+        const char *all; // 👈 'all' 변수가 없다고 하니 내부 블록에서 직접 선언합니다.
 
+        all = getenv( "LC_ALL" );
+        if (!all)
+            all = "C.UTF-8";
+
+        if (!unix_to_win_locale( all, system_locale )) system_locale[0] = 0;
+
+        if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
+        {
+            FIXME( "HACK setting EN locale.\n" );
+            all = "en_US.UTF-8";
+        }
+            
+        if (!unix_to_win_locale( all, user_locale )) user_locale[0] = 0;
+
+    }
+#else
+    if (!unix_to_win_locale ( all, user_locale )) user_locale[0] = 0;     
+    
+    if (!(all = setlocale( LC_ALL, "" )) && (all = getenv( "LC_ALL" )))
+        FIXME_(nls)( "Failed to set LC_ALL to %s, is the locale supported?\n", debugstr_a(all) );
+    if (!(ctype = setlocale( LC_CTYPE, "" )) && (ctype = getenv( "LC_CTYPE" )))
+        FIXME_(nls)( "Failed to set LC_CTYPE to %s, is the locale supported?\n", debugstr_a(ctype) );
+    if (!(messages = setlocale( LC_MESSAGES, "" )) && (messages = getenv( "LC_MESSAGES" )))
+        FIXME_(nls)( "Failed to set LC_MESSAGES to %s, is the locale supported?\n", debugstr_a(messages) );
+
+    if (!unix_to_win_locale( ctype, system_locale )) system_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_CTYPE is %s, setting system locale to %s\n", debugstr_a(ctype), debugstr_a(user_locale) );
+
+    if (main_argc > 1 && strstr(main_argv[1], "start_protected_game.exe"))
+    {
+        FIXME( "HACK setting EN locale.\n" );
+        messages = "en-US";
+    }
+
+    if (!unix_to_win_locale( messages, user_locale )) user_locale[0] = 0;
+    TRACE_(nls)( "Unix LC_MESSAGES is %s, user system locale to %s\n", debugstr_a(messages), debugstr_a(user_locale) );
+#endif
 #ifdef __APPLE__
     if (!system_locale[0])
     {
